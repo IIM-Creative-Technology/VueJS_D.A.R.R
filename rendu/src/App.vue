752ce8d304d4e3473f3b9375a28c77b7ad055df8
Raphaël <raphael.castelnau@edu.devinci.fr>
2022-03-16 18:14:46 +0100

    :rocket: new branch frontend

--- rendu/src/App.vue
+++ rendu/src/App.vue
@@ -1,26 +1,100 @@
+<script setup>
+import HeaderVue from './components/nav-1.vue'
+import Chapter1Vue from './components/Chapter1.vue'
+import Chapter2Vue from './components/Chapter2.vue'
+import Chapter3Vue from './components/Chapter3.vue'
+import Chapter4Vue from './components/Chapter4.vue'
+import Chapter5Vue from './components/Chapter5.vue'
+
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<HeaderVue></HeaderVue> 
+<section class="main">
+  <h1>Chapter{{count}}</h1>
+  <div class="content">
+  <Chapter1Vue></Chapter1Vue> 
+  <Chapter2Vue></Chapter2Vue> 
+  <Chapter3Vue></Chapter3Vue> 
+  <Chapter4Vue></Chapter4Vue> 
+  <Chapter5Vue></Chapter5Vue> 
+  </div>
+  <div class="end">
+      <p class="counter">{{count}}/5</p>
+      <button class="btn-next"><img src="./assets/next.png"></button>
+  </div>
+</section>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-family: poppins;
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  width: 30%;
+  height: 782px;
+  margin: 0 auto;
+  margin-top: -50px;
+  background-color: #E4E4E4;
+  display: flex;
+  justify-self: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 65px;
+}
+
+.content{
+  display: flex;
+  justify-self: center;
+  align-content: center;
+  width: 100%;
+  height: 80%;
+  background-color: red;
+}
+
+
+.end{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-self: center;
+  flex-direction: row;
+  margin: 10px;
+}
+
+.btn-next {
+  width: 72px;
+  height: 72px;
+  border: none;
+  background : none;
+  cursor: pointer;
+  margin-right: 20px;
+}
+
+.btn-next:hover{
+  background : none;
+}
+
+.btn-next > img{
+  width: 100%;
+  height: 100%;
+}
+
+.counter{
+  font-style: normal;
+  font-weight: 900;
+  font-size: 48px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+  margin-left: 20px;
 }
 </style>
---
+++ rendu/src/components/nav-1.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <a href="" class="logo"><img src="../assets/Vue.js.png" alt=""></a>
+    </nav>
+</template>
+
+<style scoped>
+.logo > img{
+    margin: 50px;
+}
+</style>
